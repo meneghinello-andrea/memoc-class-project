238665181e9de9ffd48fa1bf7494b9218fd9e1b5
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-01-17 18:23:49 +0100

    Aggiunta dei comandi console di compilazione della relazione

---
+++ documentation/clean.bat
@@ -0,0 +1,18 @@
+::Clean all the working directory
+del *.aux
+del *.glg
+del *.glo
+del *.gls
+del *.ist
+del *.lof
+del *.log
+del *.lot
+del *.out
+del *.toc
+del *.pdf
+cd Frontespiece
+del *.aux
+del *.log
+del *.pdf
+del *-frn.tex
+cd ..
