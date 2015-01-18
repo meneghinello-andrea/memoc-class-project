0318a8d91617bb67f3c8e3ccd7d38573dab00d8f
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-01-18 16:45:15 +0100

    Definzione file di compilazione e main principale

---
+++ metaeuristic/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "src/header/object/Chromosome.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
